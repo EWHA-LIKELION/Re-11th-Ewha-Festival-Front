da5a2c3fb073554618fced53af537b427a3e76da
gchaewon <conjs1@naver.com>
2023-08-25 15:10:03 +0900

    refactoring: 로그인 및 유저인증 로직 수정

--- src/components/authpage/LoginMenu.jsx
+++ src/components/authpage/LoginMenu.jsx
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // api
 import { RequestLogin, RequestProfile } from '../../api/auth';
 // redux
 import { useAppDispatch, useAppSelector } from '../../redux/store';
-import { setUser, setUserTask } from '../../redux/userSlice';
+import { setUser, setUserTask, setToken } from '../../redux/userSlice';
 import { setBooth_id } from '../../redux/boothSlice';
 // style.js & fonts
 import * as S from './LoginReigster.style';
 import { BiUser, BiLockOpen } from 'react-icons/bi';
+import { BsFillInfoCircleFill } from 'react-icons/bs';
 // components
 import TopBar from '../_common/topbar/TopBar';
 import LoadingModal from './LoadingModal';
+import Modal from '../_common/modal/Modal';
+
 const LoginMenu = () => {
   const navigate = useNavigate();
   // redux
@@ -22,63 +25,82 @@ const LoginMenu = () => {
   const [id, setID] = useState(ID);
   const [password, setPW] = useState(PW);
 
-  // loading
-  const [loading, setLoading] = useState(false);
+  // modal 관리
+  const [modal, setModal] = useState(false);
+  const contents =
+    '- 부스 유저 ID : booth / PW : 1111 \n - 일반 유저  ID : user / PW : 0000';
 
-  // Submit
-  const onSubmitAccount = e => {
-    setLoading(true);
-    e.preventDefault();
-    RequestLogin(id, password)
-      .then(res => {
-        //아이디 비밀번호 닉네임 저장
+  const openModal = () => {
+    setModal(true);
+  };
+  const closeModal = () => {
+    setModal(false);
+  };
+
+  // 토큰 쿠키 저장
+  function setCookie(name, value) {
+    var expires = '';
+    var date = new Date();
+    date.setTime(date.getTime() + 24 * 60 * 60 * 1000); // 1일간 유효
+    expires = '; expires=' + date.toUTCString();
+    document.cookie = name + '=' + value + expires + '; path=/';
+  }
+
+  // 테스트 로그인 처리
+  const testLogin = isBooth => {
+    if (isBooth == -1) {
+      console.log('예외처리');
+    } else {
+      // 1일간 유효한 토큰 쿠키에 저장
+      setCookie('token', 'true');
+      dispatch(
+        setUser({
+          ID: id,
+          PW: password,
+          nickname: isBooth ? '부스 관리자1' : '일반 유저1',
+        }),
+      );
+      dispatch(
+        setUserTask({
+          isBooth: isBooth,
+        }),
+      );
+      // 부스 유저 로그인인 경우 부스 아이디 저장
+      if (isBooth) {
         dispatch(
-          setUser({
-            ID: id,
-            PW: password,
-            nickname: res.data.data.nickname,
+          setBooth_id({
+            booth_id: 1,
           }),
         );
-        // 프로필 저장
-        RequestProfile(res.data.data.access_token).then(res => {
-          dispatch(
-            setUserTask({
-              isBooth: res.data.data.is_booth,
-              isTF: res.data.data.is_tf,
-            }),
-          );
-          // 부스 유저인경우 부스 아이디 저장
-          if (res.data.data.is_booth) {
-            // test
-            dispatch(
-              setBooth_id({
-                booth_id: res.data.data.booth_id,
-              }),
-            );
-            window.localStorage.setItem('booth_id', res.data.data.booth_id);
-          }
-        });
-      })
-      .then(() => {
-        setTimeout(() => {
-          setLoading(false);
-        }, 500);
-      })
-      .then(() => {
-        alert('로그인에 성공했습니다.');
-        navigate('/');
-        window.location.reload();
-      })
-      .catch(error => {
-        setLoading(false);
-        alert('아이디와 비밀번호를 확인해주세요.');
-      });
+      }
+      navigate('/');
+    }
+  };
+  // Submit
+  const onSubmitAccount = e => {
+    e.preventDefault();
+
+    (id == 'booth') & (password == '1111')
+      ? testLogin(true) // 부스 유저
+      : (id == 'user') & (password == '0000')
+      ? testLogin(false) // 일반 유저
+      : testLogin(-1); // 예외
   };
 
   return (
     <>
       <S.Container>
-        {loading ? <LoadingModal /> : null}
+        {modal && (
+          <Modal
+            open={openModal}
+            close={closeModal}
+            title='테스트 계정 안내'
+            subTitle='아래 계정 정보로 사이트 이용이 가능합니다.'
+            contents={contents}
+            secret='true'
+            onClick={openModal}
+          />
+        )}
         <TopBar title='로그인' />
         <S.LogoBox />
         <S.InputForm onSubmit={onSubmitAccount}>
@@ -100,9 +122,10 @@ const LoginMenu = () => {
             />
           </S.InputWrapper>
           <S.Button type='submit'>로그인</S.Button>
-          <S.GoRegister>
-            <a href='/auth/register'>회원가입</a>
-          </S.GoRegister>
+          <S.InfoWrapper onClick={openModal}>
+            <div>테스트 계정 안내</div>
+            <BsFillInfoCircleFill color='#FF9FC7' />
+          </S.InfoWrapper>
         </S.InputForm>
       </S.Container>
     </>
